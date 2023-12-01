991095b21193581203c0b5f903a905c930589357
Kuogee Hsieh <quic_khsieh@quicinc.com>
2023-12-01 15:19:48 -0800

    drm/msm/dp: delete EV_HPD_INIT_SETUP
    EV_HPD_INIT_SETUP flag is used to trigger the initialization of external
    DP host controller. Since external DP host controller initialization had
    been incorporated into pm_runtime_resume(), this flag became obsolete.
    msm_dp_irq_postinstall() which triggers EV_HPD_INIT_SETUP event is
    obsoleted accordingly.
    
    Changes in v4:
    -- reworded commit text
    -- drop EV_HPD_INIT_SETUP
    -- drop msm_dp_irq_postinstall()
    
    Changes in v3:
    -- drop EV_HPD_INIT_SETUP and msm_dp_irq_postinstall()
    
    Signed-off-by: Kuogee Hsieh <quic_khsieh@quicinc.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/msm/msm_drv.h
+++ drivers/gpu/drm/msm/msm_drv.h
@@ -386,7 +386,6 @@ int __init msm_dp_register(void);
 void __exit msm_dp_unregister(void);
 int msm_dp_modeset_init(struct msm_dp *dp_display, struct drm_device *dev,
 			 struct drm_encoder *encoder);
-void msm_dp_irq_postinstall(struct msm_dp *dp_display);
 void msm_dp_snapshot(struct msm_disp_state *disp_state, struct msm_dp *dp_display);
 
 void msm_dp_debugfs_init(struct msm_dp *dp_display, struct drm_minor *minor);
@@ -405,10 +404,6 @@ static inline int msm_dp_modeset_init(struct msm_dp *dp_display,
 				       struct drm_encoder *encoder)
 {
 	return -EINVAL;
-}
-
-static inline void msm_dp_irq_postinstall(struct msm_dp *dp_display)
-{
 }
 
 static inline void msm_dp_snapshot(struct msm_disp_state *disp_state, struct msm_dp *dp_display)
