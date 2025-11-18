4b78b85084a45dfb64185e2a4f5243fe9436c20b
Petr Hodina <petr.hodina@protonmail.com>
2025-11-18 22:04:17 +0100

    Input: stmfts - add optional reset GPIO support
    Add support for an optional "reset-gpios" property. If present, the
    driver drives the reset line high at probe time and releases it during
    power-on, after the regulators have been enabled.
    
    The corresponding Device Tree binding is updated to document the new
    "reset-gpios" property.
    
    Signed-off-by: Petr Hodina <petr.hodina@protonmail.com>
    Co-developed-by: David Heidelberg <david@ixit.cz>
    Signed-off-by: David Heidelberg <david@ixit.cz>

--- drivers/input/touchscreen/stmfts.c
+++ drivers/input/touchscreen/stmfts.c
@@ -77,6 +77,7 @@ static const struct regulator_bulk_data stmfts_supplies[] = {
 struct stmfts_data {
 	struct i2c_client *client;
 	struct input_dev *input;
+	struct gpio_desc *reset_gpio;
 	struct led_classdev led_cdev;
 	struct mutex mutex;
 
@@ -549,6 +550,11 @@ static int stmfts_power_on(struct stmfts_data *sdata)
 	if (err)
 		return err;
 
+	if (sdata->reset_gpio) {
+		usleep_range(1000, 2000);
+		gpiod_set_value_cansleep(sdata->reset_gpio, 0);
+	}
+
 	/*
 	 * The datasheet does not specify the power on time, but considering
 	 * that the reset time is < 10ms, I sleep 20ms to be sure
@@ -607,6 +613,10 @@ static void stmfts_power_off(void *data)
 	struct stmfts_data *sdata = data;
 
 	disable_irq(sdata->client->irq);
+
+	if (sdata->reset_gpio)
+		gpiod_set_value_cansleep(sdata->reset_gpio, 1);
+
 	regulator_bulk_disable(ARRAY_SIZE(stmfts_supplies),
 			       sdata->supplies);
 }
@@ -662,6 +672,13 @@ static int stmfts_probe(struct i2c_client *client)
 					    &sdata->supplies);
 	if (err)
 		return err;
+
+	sdata->reset_gpio = devm_gpiod_get_optional(dev, "reset",
+						    GPIOD_OUT_HIGH);
+
+	if (IS_ERR(sdata->reset_gpio))
+		return dev_err_probe(dev, PTR_ERR(sdata->reset_gpio),
+				     "Failed to get GPIO 'reset'\n");
 
 	sdata->input = devm_input_allocate_device(dev);
 	if (!sdata->input)
